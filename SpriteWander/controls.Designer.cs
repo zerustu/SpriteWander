d851386eb4a78d71411253c092a2d53f1d7c7bd5
zerustu <wasquel.valentin@gmail.com>
2023-07-19 09:26:56 +0200

    Redo Controls forms : more reactive

--- SpriteWander/controls.Designer.cs
+++ SpriteWander/controls.Designer.cs
@@ -28,68 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            EntityList = new ListBox();
+            tableLayoutPanel2 = new TableLayoutPanel();
             AddButton = new Button();
             CloseButton = new Button();
-            EntityList = new ListBox();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
             SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.AutoSize = true;
+            tableLayoutPanel1.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(EntityList, 0, 0);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 1);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.GrowStyle = TableLayoutPanelGrowStyle.FixedSize;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            tableLayoutPanel1.Size = new Size(284, 361);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // EntityList
+            // 
+            EntityList.CausesValidation = false;
+            EntityList.Dock = DockStyle.Fill;
+            EntityList.FormattingEnabled = true;
+            EntityList.ItemHeight = 15;
+            EntityList.Location = new Point(0, 0);
+            EntityList.Margin = new Padding(0);
+            EntityList.Name = "EntityList";
+            EntityList.SelectionMode = SelectionMode.MultiExtended;
+            EntityList.Size = new Size(284, 311);
+            EntityList.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Controls.Add(AddButton, 0, 0);
+            tableLayoutPanel2.Controls.Add(CloseButton, 1, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.GrowStyle = TableLayoutPanelGrowStyle.FixedSize;
+            tableLayoutPanel2.Location = new Point(3, 314);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(278, 44);
+            tableLayoutPanel2.TabIndex = 1;
             // 
             // AddButton
             // 
-            AddButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
-            AddButton.Location = new Point(20, 875);
-            AddButton.Margin = new Padding(11, 8, 11, 8);
+            AddButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            AddButton.Dock = DockStyle.Fill;
+            AddButton.Location = new Point(3, 3);
             AddButton.Name = "AddButton";
-            AddButton.Size = new Size(344, 191);
+            AddButton.Size = new Size(133, 38);
             AddButton.TabIndex = 0;
-            AddButton.Text = "Add Sprites";
+            AddButton.Text = "Add Entities";
             AddButton.UseVisualStyleBackColor = true;
             AddButton.Click += AddButton_Click;
             // 
             // CloseButton
             // 
-            CloseButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
-            CloseButton.Location = new Point(401, 875);
-            CloseButton.Margin = new Padding(11, 8, 11, 8);
+            CloseButton.AutoSize = true;
+            CloseButton.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            CloseButton.Dock = DockStyle.Fill;
+            CloseButton.Location = new Point(142, 3);
             CloseButton.Name = "CloseButton";
-            CloseButton.Size = new Size(376, 191);
+            CloseButton.Size = new Size(133, 38);
             CloseButton.TabIndex = 1;
-            CloseButton.Text = "Close app";
+            CloseButton.Text = "Close App";
             CloseButton.UseVisualStyleBackColor = true;
             CloseButton.Click += CloseButton_Click;
             // 
-            // EntityList
-            // 
-            EntityList.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            EntityList.FormattingEnabled = true;
-            EntityList.ItemHeight = 41;
-            EntityList.Location = new Point(23, 16);
-            EntityList.Margin = new Padding(11, 8, 11, 8);
-            EntityList.Name = "EntityList";
-            EntityList.SelectionMode = SelectionMode.MultiExtended;
-            EntityList.Size = new Size(735, 824);
-            EntityList.TabIndex = 2;
-            EntityList.SelectedIndexChanged += EntityList_SelectedIndexChanged;
-            // 
             // Controls
             // 
-            AutoScaleDimensions = new SizeF(17F, 41F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(797, 1083);
-            Controls.Add(EntityList);
-            Controls.Add(CloseButton);
-            Controls.Add(AddButton);
-            Margin = new Padding(11, 8, 11, 8);
+            ClientSize = new Size(284, 361);
+            Controls.Add(tableLayoutPanel1);
+            Margin = new Padding(5, 3, 5, 3);
+            MinimumSize = new Size(196, 110);
             Name = "Controls";
             Text = "controls";
             FormClosing += CloseMainApp;
             Load += Controls_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
+        private TableLayoutPanel tableLayoutPanel1;
+        private ListBox EntityList;
+        private TableLayoutPanel tableLayoutPanel2;
         private Button AddButton;
         private Button CloseButton;
-        private ListBox EntityList;
     }
 }
